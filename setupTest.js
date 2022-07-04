94c8ffea86878355f860318819dc2bb8d31ae45c
Georgy Grechko <georgre4ko@ya.ru>
2022-07-04 15:09:03 +0300

    prettier test

---
+++ setupTest.js
@@ -0,0 +1,18 @@
+global.requestAnimationFrame = (cb) => {
+    setTimeout(cb, 0)
+}
+
+global.matchMedia =
+    window.matchMedia ||
+    function () {
+        return {
+            matches: false,
+            addListener: () => {},
+            removeListener: () => {},
+        }
+    }
+
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
