fbd84a0fa9ce769fd67742a2789f03e6306c5455
Georgy Grechko <georgre4ko@ya.ru>
2022-07-04 18:32:54 +0300

    mics

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,69 +1,69 @@
 'use script'
-var loginURL = "localhost";
-var logoutURL = "/WebServices/LogOff";
-var userAgent = navigator.userAgent.toLowerCase();
-var firstLogIn = true;
+var loginURL = 'localhost'
+var logoutURL = '/WebServices/LogOff'
+var userAgent = navigator.userAgent.toLowerCase()
+var firstLogIn = true
 
 function ping(host, port) {
+    var started = new Date().getTime()
 
-    var started = new Date().getTime();
+    var http = new XMLHttpRequest()
 
-    var http = new XMLHttpRequest();
-
-    http.open("GET", "http://" + host + ":" + port, /*async*/ true);
+    http.open('GET', 'http://' + host + ':' + port, /*async*/ true)
     http.onreadystatechange = function () {
         if (http.readyState == 4) {
-            var ended = new Date().getTime();
+            var ended = new Date().getTime()
 
-            var milliseconds = ended - started;
+            var milliseconds = ended - started
 
-            window.alert(host + " ping : " + milliseconds);
+            window.alert(host + ' ping : ' + milliseconds)
         }
-    };
+    }
     try {
-        http.send(null);
+        http.send(null)
     } catch (exception) {
         // this is expected
     }
-
 }
 
 function ping1() {
-    ping("localhost", 8080);
+    ping('localhost', 8080)
 }
 
-function cyrb53(str, seed = 0) {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return 4294967296 * (2097151 & h2) + (h1>>>0);
-};
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message)
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+
+    // convert bytes to hex string
+    const hashHex = hashArray.map((b) => ('00' + b.toString(16)).slice(-2)).join('')
+    console.log(hashHex)
+    return hashHex
+}
 
 var login = function () {
-    var form = document.forms[0];
-    var username = form.username.value;
-    var pswd = form.password.value;
-    var submitBtn = document.getElementById('submit');
-    var xhr = new XMLHttpRequest();
+    var form = document.forms[0]
+    var username = form.username.value
+    var pswd = form.password.value
+    var submitBtn = document.getElementById('submit')
+    var xhr = new XMLHttpRequest()
 
     var json = JSON.stringify({
         name: username,
-        password: cyrb53(password)
-    });
+        password: sha256(password),
+    })
 
     xhr.open('POST', 'https://localhost:8080', true)
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8')
 
-    xhr.onreadystatechange = {
-
-    };
+    xhr.onreadystatechange = {}
 
     // Отсылаем объект в формате JSON и с Content-Type application/json
     // Сервер должен уметь такой Content-Type принимать и раскодировать
-    xhr.send(json);
+    xhr.send(json)
 }
---
+++ frontend/js/todolist.js
@@ -0,0 +1,17 @@
+'use strict'
+
+window.onload = function () {
+    bootlint.showLintReportForCurrentDocument([], {
+        hasProblems: false,
+        problemFree: false,
+    })
+    const inpts = document.getElementsByClassName('edit-todo-input')
+    for (var i = 0; i < inpts.length; i++) {
+        inpts[i].addEventListener('click', () => {
+            inpts[0].readOnly = false
+        })
+        inpts[i].addEventListener('mouseout', () => {
+            inpts[0].classList.add('bg-transparent')
+        })
+    }
+}
