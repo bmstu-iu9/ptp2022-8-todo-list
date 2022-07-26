87b67123f414eeb289761e02a3bc88f239997bf5
Denis <denok629@gmail.com>
2022-07-26 18:08:08 +0300

    Добавил заглушки для дальнейшей работы с БД

---
+++ backend/internal/items/repository.go
@@ -0,0 +1,34 @@
+package items
+
+import (
+	"database/sql"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/entity"
+	"log"
+)
+
+// Repository encapsulates the logic to access items from the data source.
+type Repository interface {
+	// GetAll returns all items in the application.
+	GetAll() error
+	// GetOne returns user's item with specified id.
+	GetOne(user *entity.User, item *entity.Item) error
+	// Modify modifies the users`s item with specified id.
+	Modify(user *entity.User, item *entity.Item) error
+}
+
+type repository struct {
+	db     *sql.DB
+	logger *log.Logger
+}
+
+func (repo *repository) GetAll() error {
+	return nil
+}
+
+func (repo *repository) GetOne(user *entity.User, item *entity.Item) error {
+	return nil
+}
+
+func (repo *repository) Modify(user *entity.User, item *entity.Item) error {
+	return nil
+}
