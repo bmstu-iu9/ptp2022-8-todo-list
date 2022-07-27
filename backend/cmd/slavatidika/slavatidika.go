e563130b88d7d2eaecc78d1a4a0993213c51da9f
Starovoytov Alexandr <saniy200311@gmail.com>
2022-07-27 21:32:55 +0300

    Регистрация хендлеров для эндпоинтов users

--- backend/cmd/slavatidika/slavatidika.go
+++ backend/cmd/slavatidika/slavatidika.go
@@ -6,13 +6,26 @@ import (
 	"net/http"
 
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/config"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/db"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/logger"
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/ping"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/users"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	db, err := db.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger := logger.New()
 	mux := httprouter.New()
+
 	ping.RegisterHandlers(mux)
+	users.RegisterHandlers(
+		mux,
+		users.NewService(users.NewRepository(db, logger)),
+		logger)
 
 	server := http.Server{
 		Addr:    fmt.Sprintf("%v:%v", config.Host, config.Port),
