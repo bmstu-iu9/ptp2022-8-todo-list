2ffb8740ebd58ba35a46e2c7d84876f397219f6a
Denis <denok629@gmail.com>
2022-07-29 20:57:38 +0300

    Обновил структуру Item

--- backend/internal/entity/item.go
+++ backend/internal/entity/item.go
@@ -2,6 +2,11 @@ package entity
 
 //Struct Item describing one item
 type Item struct {
-	ItemId   int    `json:"ItemId"`
-	ItemName string `json:"ItemName"`
+	ItemId      int    `json:"itemId"`
+	ItemName    string `json:"itemName"`
+	ImageSrc    string `json:"imageSrc"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Category    string `json:"category"`
+	Rarity      string `json:"rarity"`
 }
