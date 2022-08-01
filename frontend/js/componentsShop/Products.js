743f22c939cbb2f341c25250648032e122df11a8
Slava <Slava.lokshin@mail.ru>
2022-08-01 20:50:49 +0700

    Начал создание магазина как подгрузка с файла js -Очень сыро подгружается раздел "Одежда" (то ли еще будет :3 )

--- frontend/js/componentsShop/Products.js
+++ frontend/js/componentsShop/Products.js
@@ -1,10 +1,49 @@
 class Products {
 
     render() {
+        let htmlCatalog = '';
         // eslint-disable-next-line no-undef
-        CATALOG.forEach((element) => {
-            console.log(element)
-            })
+        CATALOG_SHOP.forEach(({id, name, description, imageSrc, category}) => {
+
+            if (category === 'armor' && id === 1) {
+                htmlCatalog += `
+                <div class="padding-right px-3 padding-left px-3">
+                    <h3 class="display-4 text-center" id="clothes">
+                        <a href="#top" style = "text-decoration: none">Одежда</a></h3>
+                    <br>
+                    <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-4">
+                `;
+            }
+            htmlCatalog += `
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="${imageSrc}" class="card-img-top" alt="...">
+<!--                           подумать над заданием стиля-->
+<!--                           подумать над добавлением заголовков -->
+                        <div class="card-body" style="background: #C8C8C8">
+                            <h5 class="card-title">${name}</h5>
+                                <p class="card-text">${description}</p>
+                        </div>
+                            <a href="" class="btn btn-primary">Buy</a>
+                    </div>
+                </div>
+            `;
+            if (category === 'armor' && id === 12) {
+                htmlCatalog += `
+                        </div>
+                    </div>
+                    <br>
+                `;
+            }
+
+        });
+
+        const html = `
+                ${htmlCatalog}
+        `;
+
+        const ROOT_PRODUCTS = document.getElementById('products');
+        ROOT_PRODUCTS.innerHTML = html;
     }
 }
 
--- frontend/js/constants/catalog.js
+++ frontend/js/constants/catalog.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const CATALOG = [
+const CATALOG_SHOP = [
     {
         id: 1,
         name: "Кожаный шлем",
