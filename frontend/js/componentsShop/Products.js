e79dc07f9cc1afd5a8d7df0cd17390f32ad69db8
Slava <Slava.lokshin@mail.ru>
2022-08-02 23:39:40 +0700

    Изменил теги заголовков под документацию и соеденил блок создания заголовков

--- frontend/js/componentsShop/Products.js
+++ frontend/js/componentsShop/Products.js
@@ -13,51 +13,29 @@ class Products {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         let mythical = 'linear-gradient(#40E0D0, #FF8C00, #FF0080)';
 
-        let numberBlock = 1;
+        let numberBlock = 0;
+        let namingBlock = ['Одежда', 'Аксессуары', 'Питомцы', 'Облик'];
 
         CATALOG_SHOP.forEach(({id, name, description, imageSrc, category, rarity}) => {
 
-            if (category === 'armor' && numberBlock === 1) {
-                numberBlock += 1;
+            if (category === 'armor' && numberBlock === 0 ||
+                category === 'weapon' && numberBlock === 1 ||
+                category === 'pet' && numberBlock === 2 ||
+                category === 'skin' && numberBlock === 3) {
+
                 htmlCatalog += `
                 <div class="padding-right px-3 padding-left px-3">
-                    <h3 class="display-4 text-center" id="clothes">
-                        <a href="#top" style = "text-decoration: none">Одежда</a></h3>
+                    <h3 class="display-4 text-center" id="${category}">
+                        <a href="#top" style = "text-decoration: none">${namingBlock[numberBlock]}</a></h3>
                     <br>
                     <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-4">
                 `;
-            }
-            else if (category === 'weapon' && numberBlock === 2) {
                 numberBlock += 1;
-                htmlCatalog += `
-                    <h3 class="display-4 text-center" id="accessories">
-                        <a href="#top" style="text-decoration: none">Аксессуары</a></h3>
-                    <br>
-                    <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-4">
-                `;
-            }
-            else if (category === 'pet' && numberBlock === 3) {
-                numberBlock += 1;
-                htmlCatalog += `
-                    <h3 class="display-4 text-center" id="pet">
-                        <a href="#top" style = "text-decoration: none">Питомцы</a></h3>
-                    <br>
-                    <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-4">
-                `;
-            }
-            else if (category === 'skin' && numberBlock === 4) {
-                numberBlock += 1;
-                htmlCatalog += `
-                    <h3 class="display-4 text-center" id="other">
-                        <a href="#top" style = "text-decoration: none">Облик</a></h3>
-                    <br>
-                    <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-4">
-                `;
             }
             htmlCatalog += `
                 <div class="col">
                     <div class="card h-100">
-                        <img src="http://grechkogv.ru:3000/assets/${imageSrc}" class="card-img-top" alt="...">
+                        <img src="http://grechkogv.ru:3000/assets/${imageSrc}" class="card-img-top" alt="${imageSrc}">
                         <div class="card-body" style="background: ${eval(`${rarity}`)}">
                             <h5 class="card-title">${name}</h5>
                                 <p class="card-text">${description}</p>
