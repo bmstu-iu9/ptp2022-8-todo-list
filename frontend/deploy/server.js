53525503bf4ed12a98ffbd064a3fe73626159bd8
Georgy Grechko <georgre4ko@ya.ru>
2022-08-24 14:13:05 +0300

    Улучшено логирование запросов

--- frontend/deploy/server.js
+++ frontend/deploy/server.js
@@ -1,13 +1,34 @@
-import { exists, readFile } from 'fs'
-import { basename } from 'path'
-import { createServer } from 'http'
+'use strict'
+
+import fs from 'fs'
+import path from 'path'
+import http from 'http'
 
 const cache = {}
+
+let reroutePaths = new Map([
+    ['/html/spa.html', '/app'],
+    ['/html/login.html', '/login'],
+    ['', ''],
+])
+let pathsReroute = new Map([
+    ['/app', '/html/spa.html'],
+    ['/login', '/html/login.html'],
+    ['/', 'index.html'],
+    ['', ''],
+])
+if (process.env['ENV_MODE'] === 'PROD') {
+    console.log('PROD MODE, index.html set to start_page.html.')
+    pathsReroute.set('/', '/html/start_page.html')
+} else {
+    console.log('DEV MODE, index.html not modified.')
+}
 
 /**
  * lookup content type
  * infer from the extension
  * no extension would resolve in "text/plain"
+ * @param {string} fileName
  */
 function lookupContentType(fileName) {
     const ext = fileName.toLowerCase().split('.').slice(1).pop()
@@ -36,6 +57,7 @@ function lookupContentType(fileName) {
 
 /**
  * plain 404 response
+ * @param {http.ServerResponse} res
  */
 function send404(res) {
     res.writeHead(404, { 'Content-Type': 'text/plain' })
@@ -45,54 +67,115 @@ function send404(res) {
 
 /**
  * sending file response
+ * @param {http.ServerResponse} res
+ * @param {string} filePath
  */
 function sendFile(res, filePath, fileContents) {
-    res.writeHead(200, { 'Content-Type': lookupContentType(basename(filePath)) })
+    res.writeHead(200, { 'Content-Type': lookupContentType(path.basename(filePath)) })
     res.end(fileContents)
 }
 
 /**
- * serve static content
+ * plain 302 response
+ * @param {http.ServerResponse} res
+ * @param {string} url
  */
-function serveStatic(res, cache, absPath) {
-    // use cache if there is any
-    if (cache[absPath]) {
-        sendFile(res, absPath, cache[absPath])
-    } else {
-        exists(absPath, function (fileExists) {
-            // attempt to read the resource only if it exist
-            if (fileExists) {
-                readFile(absPath, function (err, data) {
-                    // not able to read the resource
-                    if (err) {
-                        send404(res)
-                    } else {
-                        cache[absPath] = data
-                        sendFile(res, absPath, data)
-                    }
-                })
-            } else {
-                // resource does not exist
-                send404(res)
-            }
-        })
-    }
+function send302(res, url) {
+    res.writeHead(302, { location: url })
+    res.end()
 }
 
+/**
+ * serve static content
+ * @param {http.ServerResponse} res
+ * @param {{}} cache
+ * @param {string} absPath
+ * @returns {Promise}
+ */
+function serveStatic(res, cache, absPath) {
+    return new Promise((resolve, reject) => {
+        let reqCode
+        // use cache if there is any
+        if (cache[absPath]) {
+            sendFile(res, absPath, cache[absPath])
+            reqCode = '200'
+            resolve(reqCode)
+        } else {
+            fs.exists(absPath, function (fileExists) {
+                // attempt to read the resource only if it exist
+                if (fileExists) {
+                    fs.readFile(absPath, function (err, data) {
+                        // not able to read the resource
+                        if (err) {
+                            reqCode = '403'
+                            send404(res)
+                            resolve(reqCode)
+                        } else {
+                            cache[absPath] = data
+                            reqCode = '200'
+                            sendFile(res, absPath, data)
+                            resolve(reqCode)
+                        }
+                    })
+                } else {
+                    // resource does not exist
+                    reqCode = '404'
+                    send404(res)
+                    resolve(reqCode)
+                }
+            })
+        }
+    })
+}
+
+/**
+ *  Starts server.
+ * @param {{path: string, port: number}} spec 
+ * @returns 
+ */
 export default function startServer(spec) {
     let { path, port } = spec
 
     // create server object
-    var server = createServer(function (req, res) {
-        // if no resource is specified use index.html
-        if (req.url === '/') {
-            const filePath = path + 'index.html'
-            serveStatic(res, cache, filePath)
-            console.log(filePath)
+    var server = http.createServer(function (req, res) {
+        let filePath
+        let handleDefault = () => {
+            filePath = path + req.url.substring(1)
+            serveStatic(res, cache, filePath).then((code) => {
+                console.log(
+                    '\x1b[32m',
+                    'GET:',
+                    '\x1b[34m',
+                    path,
+                    ' ',
+                    req.url,
+                    ' ',
+                    code === '404' ? '\x1b[31m' : '\x1b[32m',
+                    code,
+                )
+            })
+        }
+        if (reroutePaths.has(req.url)) {
+            let reroute = reroutePaths.get(req.url)
+            send302(res, reroute)
+            console.log('\x1b[32m', 'GET:', '\x1b[34m', req.url, ' ', '\x1b[33m', '302 -->', reroutePaths.get(req.url))
+        } else if (pathsReroute.has(req.url)) {
+            filePath = path + pathsReroute.get(req.url)
+            serveStatic(res, cache, filePath).then((code) => {
+                console.log(
+                    '\x1b[32m',
+                    'GET:',
+                    '\x1b[34m',
+                    path,
+                    ' ',
+                    pathsReroute.get(req.url),
+                    ' ',
+                    code === '404' ? '\x1b[31m' : '\x1b[32m',
+                    code,
+                )
+            })
         } else {
-            let filePath = path + req.url.substring(1)
-            console.log(path, ' ', req.url)
-            serveStatic(res, cache, filePath)
+            handleDefault()
         }
     })
 
