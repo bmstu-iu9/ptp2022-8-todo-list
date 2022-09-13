cec55ee1a15026aa55974832179db592877f3bc2
Denis <denok629@gmail.com>
2022-09-13 18:58:56 +0300

    Добавил TDT

--- backend/internal/items/api_test.go
+++ backend/internal/items/api_test.go
@@ -1,100 +1,114 @@
 package items
 
 import (
+	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/entity"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/errors"
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/log"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/router"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/test"
 	"github.com/julienschmidt/httprouter"
-	. "gopkg.in/check.v1"
+	"net/http"
+	"testing"
 )
 
-type ApiTestSuite struct {
+var (
 	mux    *httprouter.Router
-	writer *httptest.ResponseRecorder
-}
+	logger log.Logger
+)
 
 func init() {
-	Suite(&ApiTestSuite{})
+	mux = router.New(logger)
+	logger = log.New()
+	service := NewService(NewMockRerository())
+	RegisterHandlers(mux, service, logger)
 }
 
-func (s *ApiTestSuite) SetUpTest(c *C) {
-	s.mux = httprouter.New()
-	RegisterHandlers(s.mux, NewService(NewMockRerository()), log.New())
-}
-
-func Test(t *testing.T) { TestingT(t) }
-
-func (s *ApiTestSuite) TestGetAll(c *C) {
-	makeRequest := func(userId string, filters string) {
-		s.writer = httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "/user/"+userId+"/items"+filters, nil)
-		s.mux.ServeHTTP(s.writer, req)
+func TestApi(t *testing.T) {
+	toJson := func(data interface{}) string {
+		buf := new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(data)
+		if err != nil {
+			panic(err)
+		}
+		return buf.String()
 	}
-	makeRequest("1", "?rarity=rare")
-	c.Check(s.writer.Code, Equals, http.StatusOK)
-	got := []entity.Item{}
-	err := json.NewDecoder(s.writer.Body).Decode(&got)
-	c.Check(err, Equals, nil)
-	c.Check(got, DeepEquals, []entity.Item{
+	notFound := toJson(errors.Problem{Title: "Not found", Status: http.StatusNotFound})
+	badRequest := toJson(errors.Problem{Title: "Bad request", Status: http.StatusBadRequest, Detail: "Request body parameters validation failed"})
+	tests := []test.ApiTestCase{
 		{
-			Id:     10,
-			Name:   "sword",
-			Rarity: "rare",
+			Name: "get all ok", Method: "GET", Url: "/user/1/items",
+			WantBody: toJson([]entity.Item{
+				{
+					Id:       10,
+					Name:     "sword",
+					Rarity:   "rare",
+					Category: "weapon",
+					State:    entity.Equipped,
+				},
+				{
+					Id:       6,
+					Name:     "head",
+					Rarity:   "legendary",
+					Category: "armor",
+					State:    entity.Equipped,
+				}}),
+			WantCode: http.StatusOK,
 		},
-	})
-}
-
-func (s *ApiTestSuite) TestGetOne(c *C) {
-	makeRequest := func(userId, itemId string) {
-		s.writer = httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "/user/"+userId+"/items/"+itemId, nil)
-		s.mux.ServeHTTP(s.writer, req)
+		{
+			Name: "get all fail", Method: "GET", Url: "/user/10/items",
+			WantBody: notFound,
+			WantCode: http.StatusNotFound,
+		},
+		{
+			Name: "get all rarity filter ok", Method: "GET", Url: "/user/1/items?rarity=rare",
+			WantBody: toJson([]entity.Item{{Id: 10, Name: "sword", Rarity: "rare", Category: "weapon",
+				State: entity.Equipped}}),
+			WantCode: http.StatusOK,
+		},
+		{
+			Name: "get all rarity and category filter ok", Method: "GET", Url: "/user/1/items?rarity=rare&category=pet",
+			WantBody: toJson(nil),
+			WantCode: http.StatusOK,
+		},
+		{
+			Name: "get all category filter ok", Method: "GET", Url: "/user/1/items?category=weapon",
+			WantBody: toJson([]entity.Item{{Id: 10, Name: "sword", Rarity: "rare", Category: "weapon",
+				State: entity.Equipped}}),
+			WantCode: http.StatusOK,
+		},
+		{
+			Name: "get all rarity filter fail", Method: "GET", Url: "/user/1/items?rarity=cringe",
+			WantBody: badRequest,
+			WantCode: http.StatusBadRequest,
+		},
+		{
+			Name: "get one ok", Method: "GET", Url: "/user/1/items/10",
+			WantBody: toJson(entity.Item{Id: 10, Name: "sword", Rarity: "rare", Category: "weapon",
+				State: entity.Equipped}),
+			WantCode: http.StatusOK,
+		},
+		{
+			Name: "get one fail user id", Method: "GET", Url: "/user/2/items/10",
+			WantBody: notFound,
+			WantCode: http.StatusNotFound,
+		},
+		{
+			Name: "get one fail item id", Method: "GET", Url: "/user/1/items/3",
+			WantBody: notFound,
+			WantCode: http.StatusNotFound,
+		},
+		{
+			Name: "update ok", Method: "PATCH", Url: "/user/1/items/10",
+			Body: toJson(UpdateItemStateRequest{ItemState: entity.Inventoried}),
+			WantBody: toJson(entity.Item{Id: 10, Name: "sword", Rarity: "rare", Category: "weapon",
+				State: entity.Inventoried}),
+			WantCode: http.StatusOK,
+		},
 	}
-	makeRequest("1", "10")
-	c.Check(s.writer.Code, Equals, http.StatusOK)
-	got := entity.Item{}
-	err := json.NewDecoder(s.writer.Body).Decode(&got)
-	c.Check(err, Equals, nil)
-	c.Check(got, DeepEquals, entity.Item{
-		Id:     10,
-		Name:   "sword",
-		Rarity: "rare",
-	})
-	makeRequest("4", "10")
-	c.Check(s.writer.Code, Equals, http.StatusNotFound)
-	makeRequest("1", "5")
-	c.Check(s.writer.Code, Equals, http.StatusNotFound)
-}
-
-func (s *ApiTestSuite) TestPatch(c *C) {
-	makeRequest := func(userId, itemId, body string) {
-		s.writer = httptest.NewRecorder()
-		bodyReader := strings.NewReader(body)
-		req, _ := http.NewRequest("PATCH", "/user/"+userId+"/items/"+itemId, bodyReader)
-		s.mux.ServeHTTP(s.writer, req)
-	}
-
-	makeRequest("1", "10", `{"itemState":"equipped"}`)
-	c.Check(s.writer.Code, Equals, http.StatusOK)
-	got := entity.Item{}
-	err := json.NewDecoder(s.writer.Body).Decode(&got)
-	c.Check(err, IsNil)
-	c.Check(got, DeepEquals, entity.Item{
-		Id:     10,
-		Name:   "sword",
-		Rarity: "rare",
-		State:  entity.Equipped,
-	})
-	makeRequest("1", "2", `{"name": "test"}`)
-	c.Check(s.writer.Code, Equals, http.StatusNotFound)
-	makeRequest("10", "10", `{"name": "test"}`)
-	c.Check(s.writer.Code, Equals, http.StatusNotFound)
+	test.Endpoint(t, tests, mux)
 }
 
 type mockRepository struct {
@@ -103,13 +117,40 @@ type mockRepository struct {
 }
 
 func (m mockRepository) GetAll(userId int, filters ItemFilter) ([]entity.Item, error) {
-	ans := make([]entity.Item, 0)
-	if filters.RarityFilter != nil {
+	if m.userId != userId {
+		return nil, fmt.Errorf("no user")
+	}
+	var ans []entity.Item
+	var f bool
+	if len(filters.RarityFilter) <= 2 && filters.RarityFilter != nil {
+		f = true
+		ans = make([]entity.Item, 0)
 		for _, item := range m.data {
-			if item.Rarity == filters.RarityFilter[0] {
-				ans = append(ans, item)
+			for _, rarity := range filters.RarityFilter {
+				if item.Rarity == rarity {
+					ans = append(ans, item)
+				}
 			}
 		}
+	}
+	if len(filters.CategoryFilter) <= 2 && filters.CategoryFilter != nil {
+		var afterRarityCheck []entity.Item
+		f = true
+		if ans != nil {
+			afterRarityCheck = ans
+			ans = nil
+		} else {
+			afterRarityCheck = m.data
+		}
+		for _, item := range afterRarityCheck {
+			for _, category := range filters.CategoryFilter {
+				if item.Category == category {
+					ans = append(ans, item)
+				}
+			}
+		}
+	}
+	if f {
 		return ans, nil
 	}
 	return m.data, nil
@@ -117,14 +158,14 @@ func (m mockRepository) GetAll(userId int, filters ItemFilter) ([]entity.Item, e
 
 func (m mockRepository) GetOne(userId, itemId int) (entity.Item, error) {
 	if userId != m.userId {
-		return entity.Item{}, errors.New("No user")
+		return entity.Item{}, fmt.Errorf("no user")
 	}
 	for _, item := range m.data {
 		if item.Id == itemId {
 			return item, nil
 		}
 	}
-	return entity.Item{}, errors.New("No data")
+	return entity.Item{}, fmt.Errorf("no data")
 }
 
 func (m mockRepository) Update(userId int, item *entity.Item) error {
@@ -134,21 +175,25 @@ func (m mockRepository) Update(userId int, item *entity.Item) error {
 			return nil
 		}
 	}
-	return errors.New("No data")
+	return fmt.Errorf("no data")
 }
 
 func NewMockRerository() *mockRepository {
 	return &mockRepository{
 		data: []entity.Item{
 			{
-				Id:     10,
-				Name:   "sword",
-				Rarity: "rare",
+				Id:       10,
+				Name:     "sword",
+				Rarity:   "rare",
+				Category: "weapon",
+				State:    entity.Equipped,
 			},
 			{
-				Id:     6,
-				Name:   "head",
-				Rarity: "legendary",
+				Id:       6,
+				Name:     "head",
+				Rarity:   "legendary",
+				Category: "armor",
+				State:    entity.Equipped,
 			},
 		},
 		userId: 1,
--- backend/internal/items/service.go
+++ backend/internal/items/service.go
@@ -1,8 +1,9 @@
 package items
 
 import (
-	"errors"
+	"fmt"
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/entity"
+	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/errors"
 	"github.com/bmstu-iu9/ptp2022-8-todo-list/backend/internal/validation"
 )
 
@@ -33,15 +34,19 @@ func NewService(repo Repository) Service {
 // GetAll returns all items.
 func (s service) GetAll(userId int, filters ItemFilter) ([]entity.Item, error) {
 	if !validation.ValidateItemState(filters.StateFilter) {
-		return nil, errors.New("wrong state filter name")
+		return nil, fmt.Errorf("%w: wrong state filter name", errors.ErrValidation)
 	}
 	if !validation.ValidateItemCategory(filters.CategoryFilter) {
-		return nil, errors.New("wrong category filter name")
+		return nil, fmt.Errorf("%w: wrong category filter name", errors.ErrValidation)
 	}
 	if !validation.ValidateItemRarity(filters.RarityFilter) {
-		return nil, errors.New("wrong rarity filter name")
+		return nil, fmt.Errorf("%w: wrong rarity filter name", errors.ErrValidation)
 	}
-	return s.repo.GetAll(userId, filters)
+	items, err := s.repo.GetAll(userId, filters)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", errors.ErrNotFound, err)
+	}
+	return items, nil
 }
 
 // GetOne returns item with specified id owned by user with specified id.
@@ -52,7 +57,7 @@ func (s service) GetOne(userId, itemId int) (entity.Item, error) {
 // UpdateItemState returns item with specified id with new ItemState.
 func (s service) UpdateItemState(userId, itemId int, input *UpdateItemStateRequest) (entity.Item, error) {
 	if !validation.ValidateItemState([]entity.ItemState{input.ItemState}) {
-		return entity.Item{}, errors.New("wrong state filter name")
+		return entity.Item{}, fmt.Errorf("%w: wrong state filter name", errors.ErrValidation)
 	}
 	entityItem, err := s.GetOne(userId, itemId)
 	if err != nil {
